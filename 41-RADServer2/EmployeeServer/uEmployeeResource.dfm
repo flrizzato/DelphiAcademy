74b459b0c226f94df685628449088e22c5c7e3bd
flrizzato <fernando.rizzato@gmail.com>
2018-03-06 10:45:51 -0300

    41th

---
+++ 41-RADServer2/EmployeeServer/uEmployeeResource.dfm
@@ -0,0 +1,102 @@
+object EmployeeServerResource1: TEmployeeServerResource1
+  OldCreateOrder = False
+  Height = 498
+  Width = 649
+  object EmpCnn: TFDConnection
+    Params.Strings = (
+      
+        'Database=C:\Users\Public\Documents\Embarcadero\Studio\19.0\Sampl' +
+        'es\Data\EMPLOYEE.GDB'
+      'User_Name=sysdba'
+      'Password=masterkey'
+      'Protocol=TCPIP'
+      'Server=localhost'
+      'DriverID=IB')
+    LoginPrompt = False
+    BeforeConnect = EmpCnnBeforeConnect
+    Left = 56
+    Top = 16
+  end
+  object EmployeeTable: TFDQuery
+    Connection = EmpCnn
+    SQL.Strings = (
+      'SELECT * FROM EMPLOYEE')
+    Left = 56
+    Top = 72
+    object EmployeeTableEMP_NO: TSmallintField
+      AutoGenerateValue = arAutoInc
+      FieldName = 'EMP_NO'
+      Origin = 'EMP_NO'
+      ProviderFlags = [pfInUpdate, pfInWhere, pfInKey]
+    end
+    object EmployeeTableFIRST_NAME: TStringField
+      FieldName = 'FIRST_NAME'
+      Origin = 'FIRST_NAME'
+      Required = True
+      Size = 15
+    end
+    object EmployeeTableLAST_NAME: TStringField
+      FieldName = 'LAST_NAME'
+      Origin = 'LAST_NAME'
+      Required = True
+    end
+    object EmployeeTablePHONE_EXT: TStringField
+      FieldName = 'PHONE_EXT'
+      Origin = 'PHONE_EXT'
+      Size = 4
+    end
+    object EmployeeTableHIRE_DATE: TSQLTimeStampField
+      AutoGenerateValue = arDefault
+      FieldName = 'HIRE_DATE'
+      Origin = 'HIRE_DATE'
+    end
+    object EmployeeTableDEPT_NO: TStringField
+      FieldName = 'DEPT_NO'
+      Origin = 'DEPT_NO'
+      Required = True
+      FixedChar = True
+      Size = 3
+    end
+    object EmployeeTableJOB_CODE: TStringField
+      FieldName = 'JOB_CODE'
+      Origin = 'JOB_CODE'
+      Required = True
+      Size = 5
+    end
+    object EmployeeTableJOB_GRADE: TSmallintField
+      FieldName = 'JOB_GRADE'
+      Origin = 'JOB_GRADE'
+      Required = True
+    end
+    object EmployeeTableJOB_COUNTRY: TStringField
+      FieldName = 'JOB_COUNTRY'
+      Origin = 'JOB_COUNTRY'
+      Required = True
+      Size = 15
+    end
+    object EmployeeTableSALARY: TBCDField
+      FieldName = 'SALARY'
+      Origin = 'SALARY'
+      Required = True
+      Precision = 18
+      Size = 2
+    end
+    object EmployeeTableFULL_NAME: TStringField
+      AutoGenerateValue = arDefault
+      FieldName = 'FULL_NAME'
+      Origin = 'FULL_NAME'
+      ProviderFlags = []
+      ReadOnly = True
+      Size = 37
+    end
+  end
+  object FDGUIxWaitCursor1: TFDGUIxWaitCursor
+    Provider = 'Console'
+    Left = 144
+    Top = 16
+  end
+  object FDPhysIBDriverLink1: TFDPhysIBDriverLink
+    Left = 248
+    Top = 16
+  end
+end
---
+++ 41-RADServer2/EmployeeServer/uEmployeeResource.pas
@@ -0,0 +1,92 @@
+unit uEmployeeResource;
+
+// EMS Resource Module
+
+interface
+
+uses
+  System.SysUtils, System.Classes, System.JSON,
+  EMS.Services, EMS.ResourceAPI, EMS.ResourceTypes, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
+  FireDAC.Phys.IB, FireDAC.Phys.IBDef, FireDAC.Stan.Param, FireDAC.DatS,
+  FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.ConsoleUI.Wait,
+  FireDAC.Stan.StorageJSON, FireDAC.Phys.IBBase, FireDAC.Comp.UI, Data.DB,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
+
+type
+
+  [ResourceName('EmployeeServer')]
+  TEmployeeServerResource1 = class(TDataModule)
+    EmpCnn: TFDConnection;
+    EmployeeTable: TFDQuery;
+    FDGUIxWaitCursor1: TFDGUIxWaitCursor;
+    FDPhysIBDriverLink1: TFDPhysIBDriverLink;
+    EmployeeTableEMP_NO: TSmallintField;
+    EmployeeTableFIRST_NAME: TStringField;
+    EmployeeTableLAST_NAME: TStringField;
+    EmployeeTablePHONE_EXT: TStringField;
+    EmployeeTableHIRE_DATE: TSQLTimeStampField;
+    EmployeeTableDEPT_NO: TStringField;
+    EmployeeTableJOB_CODE: TStringField;
+    EmployeeTableJOB_GRADE: TSmallintField;
+    EmployeeTableJOB_COUNTRY: TStringField;
+    EmployeeTableSALARY: TBCDField;
+    EmployeeTableFULL_NAME: TStringField;
+    procedure EmpCnnBeforeConnect(Sender: TObject);
+  published
+    procedure Get(const AContext: TEndpointContext;
+      const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+  end;
+
+implementation
+
+{%CLASSGROUP 'System.Classes.TPersistent'}
+
+uses DataSetConverter4D.Helper, System.IniFiles;
+
+{$R *.dfm}
+
+procedure TEmployeeServerResource1.EmpCnnBeforeConnect(Sender: TObject);
+var
+  fFileName: string;
+  fIniFile: TIniFile;
+begin
+  fFileName := IncludeTrailingPathDelimiter
+    (ExtractFilePath(GetModuleName(HInstance))) + 'EmployeeServer.ini';
+  if FileExists(fFileName) then
+  begin
+    fIniFile := TIniFile.Create(fFileName);
+    try
+      EmpCnn.Params.Database := fIniFile.ReadString('Data', 'Database', '');
+      EmpCnn.Params.UserName := fIniFile.ReadString('Data', 'UserName', '');
+      EmpCnn.Params.Password := fIniFile.ReadString('Data', 'Password', '');
+      EmpCnn.Params.Values['InstanceName'] := fIniFile.ReadString('Data',
+        'InstanceName', '');
+    finally
+      fIniFile.Free;
+    end;
+  end;
+end;
+
+procedure TEmployeeServerResource1.Get(const AContext: TEndpointContext;
+  const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
+var
+  empDataObj: TJSONObject;
+begin
+  EmployeeTable.Open;
+  empDataObj := TJSONObject.Create;
+  empDataObj.AddPair('items', EmployeeTable.AsJSONArray);
+  AResponse.Body.SetValue(empDataObj, True);
+end;
+
+procedure Register;
+begin
+  RegisterResource(TypeInfo(TEmployeeServerResource1));
+end;
+
+initialization
+
+Register;
+
+end.
